792c2ec83b54708fc0183490dc3435cca477b93d
Murali Sai Lakith <38044490+lakith-pusarla@users.noreply.github.com>
2023-11-12 01:51:55 -0500

    Resolving conflict

---
+++ TikTokTutorial/TikTokTutorial/Core/Feed/FeedCell.swift
@@ -0,0 +1,107 @@
+//
+//  FeedCell.swift
+//  TikTokTutorial
+//
+//  Created by Lakith Pusarla on 11/12/23.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    let post: Int
+    var body: some View {
+        ZStack{
+            Rectangle()
+            .fill(.pink)
+            .containerRelativeFrame([.horizontal, .vertical])
+            .overlay{
+                Text("Post ID: \(post)")
+                    .foregroundStyle(.white)
+            }
+            VStack{
+                Spacer()
+                
+                HStack(alignment: .bottom){
+                    VStack(alignment: .leading) {
+                        
+                        Text("Lakith Pusarla")
+                        
+                            .fontWeight(.semibold)
+                        
+                        Text("Jai Balayya is my anthem")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.white)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 25){
+                        
+                        Circle()
+                            .frame(width: 45, height: 45)
+                            .foregroundStyle(.gray)
+                        
+                        Button{
+                            // Action for button
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 25)
+                                .foregroundStyle(.white)
+                                
+                                Text("41")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                                
+                            }
+                        }
+                        
+                        Button{
+                            // Action for button
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipses.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28, height: 25)
+                                    .foregroundStyle(.white)
+                                
+                                Text("1718")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button{
+                            // Action for button
+                        } label: {
+                            Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 28, height: 25)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Button{
+                            // Action for button
+                        } label: {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 30, height: 25)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+                .padding(.bottom, 84)
+                
+            }
+            .padding()
+            
+        }
+    }
+}
+
+#Preview {
+    FeedCell(post: 2)
+}
