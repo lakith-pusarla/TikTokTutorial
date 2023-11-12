4c10a75c55e9757c638a7cc2040d98939a6ceae3
Murali Sai Lakith <38044490+lakith-pusarla@users.noreply.github.com>
2023-11-12 14:49:24 -0500

    Created basic Feed data model with test URLS

---
+++ TikTokTutorial/TikTokTutorial/Model/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  TikTokTutorial
+//
+//  Created by Lakith Pusarla on 11/12/23.
+//
+
+import Foundation
+
+struct Post: Identifiable, Codable{
+    let id: String
+    let videoURL: String
+}
