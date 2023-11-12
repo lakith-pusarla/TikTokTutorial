272a3a0983f0b67e8b6135d6766d9ed230ebbb6a
Murali Sai Lakith <38044490+lakith-pusarla@users.noreply.github.com>
2023-11-11 20:40:29 -0500

    Updated icons for tabs, symbol variants and tint color of TabView

--- TikTokTutorial/TikTokTutorial/Core/TabBar/MainTabView.swift
+++ TikTokTutorial/TikTokTutorial/Core/TabBar/MainTabView.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedTab = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+         TabView(selection: $selectedTab){
+            Text("Feed")
+                .tabItem {
+                    VStack{
+                        Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                            .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                        Text("Home")
+                    }
+                }
+                .onAppear{selectedTab = 0}
+                .tag(0)
+             
+            Text("Friends")
+                .tabItem {
+                    VStack{
+                        Image(systemName: selectedTab == 1 ? "person.3.fill" : "person.3")
+                            .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                        Text("Friends")
+                    }
+                }
+                .onAppear{selectedTab = 1}
+                .tag(1)
+             
+            Text("Upload posts")
+                .tabItem { Image(systemName: "plus") }
+                .onAppear{selectedTab = 2}
+                .tag(2)
+             
+             
+            Text("Notifications")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "heart")
+                            .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                        Text("Inbox")
+                    }
+                }
+                .onAppear{selectedTab = 3}
+                .tag(3)
+             
+            Text("Profile")
+                .tabItem {
+                    VStack{
+                        Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                            .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
+                        Text("Profile")
+                    }
+                }
+                .onAppear{selectedTab = 4}
+                .tag(4)
+        }
+         .tint(.black)
     }
 }
 
