4e52185c83ae580509da5287bbc75fde3cedf786
Murali Sai Lakith <38044490+lakith-pusarla@users.noreply.github.com>
2023-11-11 19:34:45 -0500

    First commit

---
+++ TikTokTutorial/TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by Lakith Pusarla on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
