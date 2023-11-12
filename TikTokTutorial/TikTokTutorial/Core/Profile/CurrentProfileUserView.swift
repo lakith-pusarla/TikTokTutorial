d7c18ebf7bc83c655046e10f5e8f6091f9f6b215
Murali Sai Lakith <38044490+lakith-pusarla@users.noreply.github.com>
2023-11-12 13:41:17 -0500

    Created ProfileView current profile header

---
+++ TikTokTutorial/TikTokTutorial/Core/Profile/CurrentProfileUserView.swift
@@ -0,0 +1,27 @@
+//
+//  CurrentProfileUserView.swift
+//  TikTokTutorial
+//
+//  Created by Lakith Pusarla on 11/12/23.
+//
+
+import SwiftUI
+
+struct CurrentProfileUserView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                VStack(spacing: 2){
+                    // Profile header
+                    
+                    
+                    
+                    // Post grid view
+                }
+            }
+        }    }
+}
+
+#Preview {
+    CurrentProfileUserView()
+}
