364ce0db8c71bc04c9f416a92f3371b5a7228027
Murali Sai Lakith <38044490+lakith-pusarla@users.noreply.github.com>
2023-11-20 00:30:29 -0500

    Adding firebase code for login screen

--- TikTokTutorial/TikTokTutorial/Core/Authentication/Service/AuthService.swift
+++ TikTokTutorial/TikTokTutorial/Core/Authentication/Service/AuthService.swift
@@ -7,3 +7,17 @@
 
 import Foundation
 import Firebase
+
+class AuthService{
+    func login(withEmail email: String, password: String) async throws{
+        
+    }
+    
+    func createUser(withEmail email: String, password: String, username: String, fullname: String) async throws{
+        
+    }
+    
+    func signout(){
+        
+    }
+}
---
+++ TikTokTutorial/TikTokTutorial/Core/Authentication/ViewModel/LoginViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewModel.swift
+//  TikTokTutorial
+//
+//  Created by Lakith Pusarla on 11/20/23.
+//
+
+import Foundation
+
+class LoginViewModel: ObservableObject{
+    
+    init(service: AuthService){
+        
+    }
+}
